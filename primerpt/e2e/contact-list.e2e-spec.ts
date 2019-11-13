63b2496f8e000fed3a25d18b91a2248092372aeb
Pau Paches Leal <ppachesl@opentext.com>
2019-11-13 16:53:11 +0100

    section 8.6.2 this is the code from the book with some typos corrected so that it works. It is different from the code in the github repository for the book but the typos were corrected by comparing the code from the book with the book's github code. The import {promise as wdpromise} from 'selenium-webdriver'; is both in the book code and in the book's code in github but is not needed.

--- primerpt/e2e/contact-list.e2e-spec.ts
+++ primerpt/e2e/contact-list.e2e-spec.ts
@@ -1,6 +1,32 @@
 import {browser, by, element} from 'protractor';
 
+
+export interface Contact {
+    name?: string;
+    email?: string;
+    tel?: string;
+}
+
+
 describe('the contact list', () => {
+
+    let expectedContactList: Contact[] = [{
+        name: 'Adrian Directive',
+        email: 'adrian.directive@example.com',
+        tel: '+1 (703) 555-0123'
+    }, {
+        name: 'Rusty Component',
+        email: 'rusty.component@example.com',
+        tel: '+1 (441) 555-0122'
+    }, {
+        name: 'Jeff Pipe',
+        email: 'jeff.pipe@example.com',
+        tel: '+1 (714) 555-0111'
+    }, {
+        name: 'Craig Service',
+        email: 'craig.services@example.com',
+        tel: '+1 (514) 555-0132'
+    }];
 
     beforeAll(() => {
         browser.get('/#/');
@@ -21,5 +47,33 @@ describe('the contact list', () => {
         expect(craigService.all(by.tagName('td'))
             .get(2).getText())
             .toBe('craig.services@example.com');
+    });
+
+    it('with map: should create a map object', () => {
+        let tbody = element(by.tagName('tbody'));
+        let trs = tbody.all(by.tagName('tr'));
+        let contactList = trs.map(elem => {
+            let contact: Contact = {};
+            let promises: any[] = [];
+            let tds = elem.all(by.tagName('td'));
+            promises.push(tds.get(1).getText().then(text => {
+                contact.name = text;
+            }));
+            promises.push(tds.get(2).getText().then(text => {
+                contact.email = text;
+            }));
+            promises.push(tds.get(3).getText().then(text => {
+                contact.tel = text;
+            }));
+
+            return Promise.all(promises).then(() => {
+                return contact;
+            });
+        });
+        expect(contactList).toBeDefined();
+        contactList.then((contacts: Contact[]) => {
+            expect(contacts.length).toEqual(4);
+            expect(contacts).toEqual(expectedContactList);
+        });
     });
 });
