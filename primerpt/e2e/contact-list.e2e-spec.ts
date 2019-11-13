072bc3e5321fde507e74bad80223704f20637f15
Pau Paches Leal <ppachesl@opentext.com>
2019-11-13 16:12:51 +0100

    section 8.6.1 add the missing beforeAll method that fixes the test.

--- primerpt/e2e/contact-list.e2e-spec.ts
+++ primerpt/e2e/contact-list.e2e-spec.ts
@@ -1,19 +1,25 @@
-import { browser, by, element } from 'protractor';
+import {browser, by, element} from 'protractor';
 
 describe('the contact list', () => {
+
+    beforeAll(() => {
+        browser.get('/#/');
+    });
+
+
     it('with filter: should find existing ' +
         'contact "Craig Service"', () => {
         let tbody = element(by.tagName('tbody'));
         let trs = tbody.all(by.tagName('tr'));
-    let craigService = trs.filter(elem => {
-        return elem.all(by.tagName('td')).get(1).getText()
-    .then(text => {
-            return text === 'Craig Service';
+        let craigService = trs.filter(elem => {
+            return elem.all(by.tagName('td')).get(1).getText()
+                .then(text => {
+                    return text === 'Craig Service';
+                });
         });
-    });
         expect(craigService.count()).toBeGreaterThan(0);
-    expect(craigService.all(by.tagName('td'))
-        .get(2).getText())
-    .toBe('craig.services@example.com');
+        expect(craigService.all(by.tagName('td'))
+            .get(2).getText())
+            .toBe('craig.services@example.com');
     });
 });
